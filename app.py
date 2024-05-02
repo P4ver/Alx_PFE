0f8023a44395aeba612e435a5eff9f488aa651f4
P4ver <s.pales20@gmail.com>
2024-05-02 01:14:14 +0100

    portfolio project

---
+++ app.py
@@ -0,0 +1,59 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///store.db'
+db = SQLAlchemy(app)
+
+# Product model
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+
+    def __repr__(self):
+        return f"Product('{self.name}', '{self.price}')"
+
+db.create_all()
+
+@app.route('/')
+def index():
+    products = Product.query.all()
+    return render_template('index.html', products=products)
+
+@app.route('/product/<int:product_id>')
+def product(product_id):
+    product = Product.query.get_or_404(product_id)
+    return render_template('product.html', product=product)
+
+@app.route('/product/add', methods=['GET', 'POST'])
+def add_product():
+    if request.method == 'POST':
+        name = request.form['name']
+        price = float(request.form['price'])
+        new_product = Product(name=name, price=price)
+        db.session.add(new_product)
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('add_product.html')
+
+@app.route('/product/<int:product_id>/update', methods=['GET', 'POST'])
+def update_product(product_id):
+    product = Product.query.get_or_404(product_id)
+    if request.method == 'POST':
+        product.name = request.form['name']
+        product.price = float(request.form['price'])
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('update_product.html', product=product)
+
+@app.route('/product/<int:product_id>/delete', methods=['POST'])
+def delete_product(product_id):
+    product = Product.query.get_or_404(product_id)
+    db.session.delete(product)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
